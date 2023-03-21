7fae0335099497d7dbf0afd385d37506b8c43a25
HSGamer <huynhqtienvtag@gmail.com>
2023-03-21 16:50:18 +0700

    cooldown in EndingState

--- src/main/java/me/hsgamer/gamesinthebox/kingofthehill/state/EndingState.java
+++ src/main/java/me/hsgamer/gamesinthebox/kingofthehill/state/EndingState.java
@@ -4,11 +4,13 @@ import me.hsgamer.gamesinthebox.game.feature.PlannerFeature;
 import me.hsgamer.gamesinthebox.game.feature.PointFeature;
 import me.hsgamer.gamesinthebox.game.simple.feature.SimpleRewardFeature;
 import me.hsgamer.gamesinthebox.kingofthehill.KingOfTheHill;
+import me.hsgamer.gamesinthebox.kingofthehill.feature.CooldownFeature;
 import me.hsgamer.gamesinthebox.util.GameUtil;
 import me.hsgamer.hscore.bukkit.utils.MessageUtils;
 import me.hsgamer.minigamecore.base.Arena;
 import me.hsgamer.minigamecore.base.GameState;
 import me.hsgamer.minigamecore.bukkit.extra.ColoredDisplayName;
+import me.hsgamer.minigamecore.implementation.feature.TimerFeature;
 import org.bukkit.Bukkit;
 
 import java.util.List;
@@ -25,16 +27,21 @@ public class EndingState implements GameState, ColoredDisplayName {
     public void start(Arena arena) {
         String endMessage = GameUtil.replaceName(expansion.getMessageConfig().getEndBroadcast(), arena);
         Bukkit.getOnlinePlayers().forEach(player -> MessageUtils.sendMessage(player, endMessage));
-    }
 
-    @Override
-    public void update(Arena arena) {
         List<UUID> topList = arena.getFeature(PointFeature.class).getTopUUID();
         if (!arena.getFeature(SimpleRewardFeature.class).tryReward(topList)) {
             String notEnoughPlayerMessage = GameUtil.replaceName(expansion.getMessageConfig().getNotEnoughPlayerToReward(), arena);
             Bukkit.getOnlinePlayers().forEach(player -> MessageUtils.sendMessage(player, notEnoughPlayerMessage));
         }
-        arena.setNextState(IdlingState.class);
+
+        arena.getFeature(CooldownFeature.class).start(this);
+    }
+
+    @Override
+    public void update(Arena arena) {
+        if (arena.getFeature(TimerFeature.class).getDuration() <= 0) {
+            arena.setNextState(IdlingState.class);
+        }
     }
 
     @Override
